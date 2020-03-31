01997bef0ddcc03e071a955cac9ab99d748bd67d
thokchom <thokchomrs@gmail.com>
2020-03-31 23:39:27 +0530

    Updates

---
+++ precache-manifest.6f4ef2d18322f8547f419c5806e36cf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6228b464bacb0746846b6b17603ac55",
+    "url": "/my-first-host/index.html"
+  },
+  {
+    "revision": "2ed048f80dae52b1e2b0",
+    "url": "/my-first-host/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5d73f92865addedf4957",
+    "url": "/my-first-host/static/js/2.beb7ff38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-first-host/static/js/2.beb7ff38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ed048f80dae52b1e2b0",
+    "url": "/my-first-host/static/js/main.28834cca.chunk.js"
+  },
+  {
+    "revision": "06cf2169bace2e299973",
+    "url": "/my-first-host/static/js/runtime-main.80301525.js"
+  }
+]);
