8a9cf68f2baf303ef1f64f62bec4374dd47c8689
thokchom <thokchomrs@gmail.com>
2020-03-31 23:54:21 +0530

    your awesome message

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container" style={{textAlign:'center', color:"green"}}> 
+     <h2>My trial App hosting</h2>
+    </div>
+  );
+}
+
+export default App;
